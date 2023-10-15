0170fbd3cf68e508de6971992b0343481fb3420b
Alok Suman <71497735+Alokray007@users.noreply.github.com>
2023-10-15 16:17:50 +0530

    Functional Components List - 1st commit

---
+++ practice-functional-components-lists-C1-S2/src/PokeMoveCard.js
@@ -0,0 +1,13 @@
+import "./PokeMoveCard.css";
+
+export const PokeMoveCard = (props) => {
+  const { id, type, move, level } = props;
+  return (
+    <li className="poke-move-card">
+      <h3>Move {id}</h3>
+      <h4 style={{ padding: 10 }}>{move.toUpperCase()}</h4>
+      <p>Type: {type}</p>
+      <p>Level: {level}</p>
+    </li>
+  );
+};
