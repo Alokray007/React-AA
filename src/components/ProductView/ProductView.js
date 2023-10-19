a72f03ec781ac8defc8a97ca28c81201538f9556
Alok Suman <71497735+Alokray007@users.noreply.github.com>
2023-10-19 19:07:01 +0530

    React Hooks - 1st commit

---
+++ src/components/ProductView/ProductView.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import ProductListItem from "../ProductListItem";
+import ProductDetails from "../ProductDetails";
+import './ProductView.css'
+import { useState } from 'react';
+
+function ProductView({ products }) {
+
+  // TODO: Replace with state variable
+  const [sideOpen, setSideOpen] = useState(true);
+  const [selectedProduct, SetSelectedProduct] = useState();
+
+  useEffect(() => {
+    console.log(`selectedProduct CHANGED TO`, selectedProduct);
+    if(selectedProduct)
+    setSideOpen(true)
+  },[selectedProduct])
+
+  useEffect(() => {
+    console.log(`sideOpen CHANGED TO`, sideOpen);
+    if(!sideOpen)
+    SetSelectedProduct();
+  },[sideOpen])
+
+  return (
+    <div className="product-view">
+      <div className="product-main-area">
+        <h1>Products</h1>
+        <div className="product-list">
+          {products.map(item =>
+            <ProductListItem
+              key={item.id}
+              product={item}
+              isSelected={selectedProduct?.id === item.id}
+              onClick={() => SetSelectedProduct(item)  }
+            />
+          )}
+        </div>
+      </div>
+      <div className="product-side-panel">
+        <div className="product-side-panel-toggle-wrapper">
+          <div className="product-side-panel-toggle"
+               onClick={() => setSideOpen(!sideOpen)}>
+            {sideOpen ? '>' : '<'}
+          </div>
+        </div>
+        <ProductDetails visible={sideOpen} product={selectedProduct} />
+      </div>
+    </div>
+  );
+}
+
+export default ProductView;
