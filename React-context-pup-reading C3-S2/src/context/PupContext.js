31aa77b5909019b4ec947978af4fcb383df603be
Alok Suman <71497735+Alokray007@users.noreply.github.com>
2023-10-26 00:11:38 +0530

    1st commit

---
+++ React-context-pup-reading C3-S2/src/context/PupContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState, useContext } from 'react'
+import speedy from '../pups/speedy-pup.jpg';
+import sleepy from "../pups/sleepy-pup.jpg";
+
+export const PupContext = createContext();
+
+export function PupProvider(props) {
+  const [puppyType, setPuppyType] = useState(sleepy);
+
+  return (
+    <PupContext.Provider value={{ puppyType, setPuppyType }}>
+      {props.children}
+    </PupContext.Provider>
+  )
+}
+
+export function usePuppyType() {
+  return useContext(PupContext);
+}
